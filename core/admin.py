22d10555a1f1bf41f05ff9c9177c04f1face6ac7
oksym <milomirsimonovic@gmail.com>
2026-08-13 16:41:16 +0200

    *postavljeni modeli

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Employee,Department
 
-# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Department)
+    
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+class Employee(models.Model):
+
+    EMPLOYEE_TYPE=[
+        ('full-time','Full-time'),('part-time', 'Part-time'),('intern','Intern')
+    ]
+
+    EMPLOYMENT_STATUS=[
+        ('active', 'Active' ),('inactive', 'Inactive')
+    ]
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    mobile_phone=models.CharField(max_length=20)
+    employee_type=models.CharField(max_length=30,choices=EMPLOYEE_TYPE) #posto vec postoje  odeljenja ja sam umesto titula kojih bi bilo mnogo ovde stavio drugi podatak
+    employment_date=models.DateField()
+    salary = models.PositiveIntegerField()
+    employment_status=models.CharField(max_length=10, choices=EMPLOYMENT_STATUS)
+    department=models.ForeignKey(Department, on_delete=models.PROTECT, related_name='employees')
+    #nisam siguran PROTECT ili DELETE, da li kad odeljenje se obrise to znaci i da su zaposleni otpusteni
+
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
